4fc4f8d46efc202d0f959a587faebfbfcb149b65
Xiuwen Zheng <zhengxwen@gmail.com>
2019-01-03 00:03:47 -0600

    update seqAssocGMMAT_SPA()

--- R/saige_main.r
+++ R/saige_main.r
@@ -98,7 +98,7 @@
 # Open a SNP GDS file
 #
 
-seqAssocGMMAT_SPA <- function(gdsfile, modobj, verbose=TRUE)
+seqAssocGMMAT_SPA <- function(gdsfile, modobj, parallel=FALSE, verbose=TRUE)
 {
     stopifnot(inherits(gdsfile, "SeqVarGDSClass"))
     stopifnot(is.logical(verbose), length(verbose)==1L)
@@ -112,34 +112,48 @@ seqAssocGMMAT_SPA <- function(gdsfile, modobj, verbose=TRUE)
     .check_saige_model(modobj)
 
     # check sample ID
-    seqSetFilter(gdsfile, sample.id=modobj$sample.id)
+    seqSetFilter(gdsfile, sample.id=modobj$sample.id, verbose=FALSE)
     sid <- seqGetData(gdsfile, "sample.id")
     if (length(sid) != length(modobj$sample.id))
-        stop("")
-    ii <- match()
+        stop("Some of sample IDs do not exist in the GDS file.")
+    ii <- match(sid, modobj$sample.id)
+    if (any(is.na(ii)))
+        stop("Sample IDs do not match.")
 
     # initialize
-    mu <- as.vector(modobj$fitted.values)
+    mu <- unname(modobj$fitted.values)
 	mobj <- list(
-        y     = modobj$obj.noK$y,
-        mu.a  = mu,
-        mu2.a = mu * (1 - mu),
-        XXVX_inv = modobj$obj.noK$XXVX_inv,
-        XV = modobj$obj.noK$XV,
+        y     = unname(modobj$obj.noK$y[ii]),
+        mu.a  = mu[ii],
+        mu2.a = (mu * (1 - mu))[ii],
+        XXVX_inv = modobj$obj.noK$XXVX_inv[ii, ],
+        XV = modobj$obj.noK$XV[, ii],
         var.ratio = mean(modobj$var.ratio, na.rm=TRUE)
 	)
+    if (!is.finite(mobj$var.ratio))
+        stop("Invalid variance ratio in the SAIGE model.")
     .Call(saige_score_test_init, mobj)
 
-    # 
+    # scan
     if (modobj$trait.type == "binary")
     {
         rv <- seqApply(gdsfile, "annotation/format/DS",
-            .cfunction("saige_score_test_bin"),
-            as.is="list", .progress=verbose, .list_dup=FALSE)
+            .cfunction("saige_score_test_bin"), as.is="list",
+            parallel=parallel, .progress=verbose, .list_dup=FALSE)
     }
 
-	return(rv)
-
-
-    invisible()
+    data.frame(
+        id  = seqGetData(f, "variant.id"),
+        chr = seqGetData(f, "chromosome"),
+        pos = seqGetData(f, "position"),
+        ref = seqGetData(f, "$ref"),
+        alt   = seqGetData(f, "$alt"),
+        AF   = sapply(rv, `[`, i=1L),
+        AC   = sapply(rv, `[`, i=2L),
+        num  = as.integer(sapply(rv, `[`, i=3L)),
+        beta = sapply(rv, `[`, i=4L),
+        SE   = sapply(rv, `[`, i=5L),
+        pval = sapply(rv, `[`, i=6L),
+        stringsAsFactors = FALSE
+    )
 }
